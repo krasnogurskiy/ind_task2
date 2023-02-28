8b2a2d3f735a1c8c834fe024e1984fb090559f5c
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-02-28 11:13:17 +0200

    оголошення базового класу, конструктори, перетворення на рядок

--- Sportsman.cs
+++ Sportsman.cs
@@ -7,13 +7,26 @@ namespace ind_task2
     class Sportsman
     {
         private string lastname;
-        private string nationality;
         private int age;
+        private string nationality;
 
         public Sportsman()
         {
             lastname = "";
+            age = 0;
+            nationality = "";
+        }
 
+        public Sportsman(string _lastname, int _age, string _nationality)
+        {
+            this.lastname = _lastname;
+            this.age = _age;
+            this.nationality = _nationality;
+        }
+
+        public override string ToString()
+        {
+            return $"{lastname}, {age}, {nationality}";
         }
     }
 }
