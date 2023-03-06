f0056735acab143cb3395e0d6f46a7be684479fd
RostykVoloschak <98826968+roha04@users.noreply.github.com>
2023-03-07 00:03:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,59 @@ namespace ind_task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            // create a list to hold the athletes
+            List<Sportsman> sportsmen = new List<Sportsman>();
+
+            // prompt the user to add athletes
+            while (true)
+            {
+                Console.WriteLine("Do you want to add a new athlete? (Y/N)");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "y")
+                {
+                    // prompt the user to enter the details of a new athlete
+                    Console.WriteLine("Enter the name of the athlete:");
+                    string lastname = Console.ReadLine();
+
+                    Console.WriteLine("Enter the age of the athlete:");
+                    int age = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the citizenship of the athlete:");
+                    string nationality = Console.ReadLine();
+
+
+                    // create a new athlete object using the input values
+                    Sportsman newSportsman = new Sportsman(lastname,age,nationality);
+
+                    // add the new athlete object to the list of athletes
+                    sportsmen.Add(newSportsman);
+                }
+                else if (answer.ToLower() == "n")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid answer. Please enter Y or N.");
+                }
+            }
+            if (sportsmen.Count == 0)
+            {
+                Console.WriteLine("No athletes");
+            }
+            else
+            {
+                // print the list of athletes
+                foreach (Sportsman sportsman in sportsmen)
+                {
+                    sportsman.age++;
+                    Console.WriteLine(sportsman.ToString());
+                }
+                
+            }
+            
         }
     }
 }
