6ac5ce7915698522c5cbc77f3dee921e75605e57
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-03-14 08:21:26 +0200

    added the TennisPlayer subclass, changes in the base class and Main

--- Program.cs
+++ Program.cs
@@ -29,12 +29,12 @@ namespace ind_task2
                     Console.WriteLine("Enter the citizenship of the athlete:");
                     string nationality = Console.ReadLine();
 
-                    Console.WriteLine("Enter the earnings of the athlete:");
-                    decimal earnings = decimal.Parse(Console.ReadLine());
+                    //Console.WriteLine("Enter the earnings of the athlete:");
+                    //decimal earnings = decimal.Parse(Console.ReadLine());
 
 
                     // create a new athlete object using the input values
-                    Sportsman newSportsman = new Sportsman(lastname,age,nationality, earnings);
+                    Sportsman newSportsman = new Sportsman(lastname,age,nationality);
 
                     // add the new athlete object to the list of athletes
                     sportsmen.Add(newSportsman);
--- Sportsman.cs
+++ Sportsman.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
@@ -10,19 +10,28 @@ namespace ind_task2
         public string lastname { get; set; }
         public int age { get; set; }
         public string nationality { get; set; }
-        public decimal earnings { get; set; }
+        
 
-        public Sportsman(string lastname, int age, string nationality, decimal earnings)
+        public Sportsman()
+        {
+            lastname = string.Empty;
+            age = 0;
+            nationality = string.Empty;
+        }
+      
+        public Sportsman(string lastname, int age, string nationality)
         {
             this.lastname = lastname;
             this.age = age;
             this.nationality = nationality;
-            this.earnings = earnings;
         }
-
+        public virtual decimal CalculateEarnings()
+        {
+            return 0;
+        }
         public override string ToString()
         {
-            return $"Name:{lastname}, Age:{age}, Nationality:{nationality}, Earnings:{earnings}";
+            return $"Name:{lastname}, Age:{age}, Nationality:{nationality}";
         }
         public static Sportsman operator ++(Sportsman sportsman)
         {
@@ -31,12 +40,42 @@ namespace ind_task2
         }
         public static bool operator >(Sportsman a, Sportsman b)
         {
-            return a.earnings > b.earnings;
+            return a.CalculateEarnings() > b.CalculateEarnings();
         }
         public static bool operator <(Sportsman a, Sportsman b)
         {
-            return a.earnings < b.earnings;
+            return a.CalculateEarnings() < b.CalculateEarnings();
         }
     }
-    
+    public class TennisPlayer : Sportsman
+    {
+        public int Ranking { get; set; }
+        public decimal Prize1 { get; set; }
+        public decimal Prize2 { get; set; }
+        public decimal Prize3 { get; set; }
+
+        public TennisPlayer() : base()
+        {
+            Ranking = 0;
+            Prize1 = 0;
+            Prize2 = 0;
+            Prize3 = 0;
+        }
+        public TennisPlayer(string lastname, int age, string nationality, int ranking, decimal prize1, decimal prize2, decimal prize3) : base(lastname, age, nationality)
+        {
+            Ranking = ranking;
+            Prize1 = prize1;
+            Prize2 = prize2;
+            Prize3 = prize3;
+        }
+        public override decimal CalculateEarnings()
+        {
+            return Prize1 + Prize2 + Prize3;
+        }
+        public override string ToString()
+        {
+            return $"Name:{lastname}, Age:{age}, Income:{CalculateEarnings()}";
+        }
+    }
 }
+
